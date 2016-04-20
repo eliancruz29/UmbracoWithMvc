dd50df6a1d46dc5bd3d0a22394ccedaee7e06b57
Elian Cruz <eliancruz29@gmail.com>
2016-04-20 15:38:49 -0400

    Finish Second Module

---
+++ UmbracoWithMvc/UmbracoWithMvc.Plugins/Controllers/MyPluginSurfaceController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace UmbracoWithMvc.Plugins.Controllers
+{
+    [PluginController("UmbracoWithMvcPlugins")]
+    public class MyPluginSurfaceController : SurfaceController
+    {
+        public ActionResult Index()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ UmbracoWithMvc/UmbracoWithMvc/Controllers/ContactUsSurfaceController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using UmbracoWithMvc.Models;
+
+namespace UmbracoWithMvc.Controllers
+{
+    public class ContactUsSurfaceController : SurfaceController
+    {
+        public ActionResult Index(ContactUsModel model)
+        {
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
